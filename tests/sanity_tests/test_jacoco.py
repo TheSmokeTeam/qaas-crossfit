fcb3945882c3064680363d674122ed39d8d79238
idan-beren <idanbe7@gmail.com>
2026-01-27 22:47:09 +0200

    test: add unit tests for Jacoco report generation and coverage merging

---
+++ tests/sanity_tests/test_jacoco.py
@@ -0,0 +1,104 @@
+import os.path
+from pathlib import Path
+
+import pytest
+
+import crossfit
+from crossfit import Jacoco, Command
+from crossfit.executors import LocalExecutor
+from crossfit.models import CommandResult, ReportFormat
+from tests import logger, tests_dir_path
+
+
+@pytest.fixture
+def jacoco_tool():
+    return Jacoco(logger, crossfit.refs.tools_dir, True)
+
+@pytest.fixture
+def local_executor():
+    return LocalExecutor(logger, False, **{"check": True})
+
+@pytest.fixture
+def coverage_files():
+    return [Path(str(os.path.relpath(tests_dir_path / r"tests_helpers/tools/jacoco/f1.exec"))),
+            Path(str(os.path.relpath(tests_dir_path / r"tests_helpers/tools/jacoco/f2.exec")))]
+
+
+@pytest.fixture
+def target_dir():
+    return Path(str(os.path.relpath(tests_dir_path / r"tests_helpers/tools/jacoco/output/")))
+
+
+@pytest.fixture
+def sourcecode_dir():
+    return Path(str(os.path.relpath(tests_dir_path / r"tests_helpers/tools/jacoco/sourcecode/")))
+
+
+@pytest.fixture
+def classfiles_dir():
+    return Path(str(os.path.relpath(tests_dir_path / r"tests_helpers/tools/jacoco/classfiles/")))
+
+
+@pytest.mark.parametrize("report_format, report_formats, expected_return_code", [
+    (ReportFormat.Csv, [ReportFormat.Html, ReportFormat.Xml], 0),
+    (ReportFormat.Html, None, 0),
+    (ReportFormat.Xml, [], 0),
+], ids=[
+    "Csv_Report_With_Html_And_Xml",
+    "Html_Report",
+    "Xml_Report_Only",
+])
+def test_jacoco_execute_report(jacoco_tool, local_executor, coverage_files, target_dir, sourcecode_dir, classfiles_dir,
+                               report_format, report_formats, expected_return_code):
+    extras = [("--quiet", None), ("--tabwith", "6")]
+
+    command = jacoco_tool.save_report(
+        coverage_files,
+        target_dir,
+        report_format,
+        report_formats,
+        sourcecode_dir,
+        classfiles_dir,
+        *extras
+    )
+
+    assert isinstance(command, Command)
+
+    result = local_executor.execute(command)
+    assert isinstance(result, CommandResult)
+    assert result.code == expected_return_code
+
+
+def test_jacoco_execute_report_files_wildcard(jacoco_tool, local_executor, target_dir, sourcecode_dir, classfiles_dir):
+    report_format = ReportFormat.Csv
+    coverage_files = [Path(tests_dir_path / r"tests_helpers/tools/jacoco/*.exec")]
+    report_formats = [ReportFormat.Html, ReportFormat.Xml]
+    extras = [("--quiet", None), ("--tabwith", "6")]
+
+    command = jacoco_tool.save_report(
+        coverage_files,
+        target_dir,
+        report_format,
+        report_formats,
+        sourcecode_dir,
+        classfiles_dir,
+        *extras
+    )
+
+    assert isinstance(command, Command)
+
+    result = local_executor.execute(command)
+    assert isinstance(result, CommandResult)
+    assert result.code == 0
+
+
+def test_jacoco_merge_coverage(jacoco_tool, local_executor, coverage_files, target_dir):
+    target_file = Path("merged.exec")
+    extras = []
+
+    command = jacoco_tool.merge_coverage(coverage_files, target_dir, target_file, *extras)
+    assert isinstance(command, Command)
+
+    result = local_executor.execute(command)
+    assert isinstance(result, CommandResult)
+    assert result.code == 0
