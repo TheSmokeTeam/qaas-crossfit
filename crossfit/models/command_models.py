2fc5b887f212de45a4fa1a35f84c2c38cd19e414
idan-beren <idanbe7@gmail.com>
2026-01-29 12:16:23 +0200

    refactor: update CI configuration and improve type hints in command models

--- crossfit/models/command_models.py
+++ crossfit/models/command_models.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Union
+from typing import Optional
 
 from pydantic import BaseModel
 
@@ -14,11 +14,10 @@ class CommandType(Enum):
 class CommandResult(BaseModel):
     code: int
     command: str
-    output: Union[str, None] = ""
-    target: Union[str, None] = ""
-    error: Union[str, None] = ""
+    output: Optional[str] = ""
+    target: Optional[str] = ""
+    error: Optional[str] = ""
 
-    # exception: Union[str, None] = None
 
     def __add__(self, other):
         self.code &= other.code
@@ -26,7 +25,6 @@ class CommandResult(BaseModel):
         self.output = "\n".join(filter(lambda val: val is not None, (self.output, other.output)))
         self.target = other.target or self.target
         self.error = "\n".join(filter(lambda val: val is not None, (self.error, other.error)))
-        # self.exception = other.exception if other.exception is not None else self.exception
         return self
 
     def add_result(self, other):
